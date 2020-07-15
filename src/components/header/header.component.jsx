0faacce5d7a9fe4a9b89575688cbb9b9feddd2ac
iggy <iggy.uga@gmail.com>
2020-07-15 11:50:57 -0400

    build signin page, signin, form-input, custom-button components

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -12,7 +12,8 @@ const Header = () => (
         </Link>
         <div className='options'>
             <Link className='option' to='/shop'>SHOP</Link>
-            <Link className='option' to='/shop'>CONTACT</Link>
+            <Link className='option' to='/contact'>CONTACT</Link>
+            <Link className='option' to='/signin'>SIGNIN</Link>
         </div>
     </div>
 )
